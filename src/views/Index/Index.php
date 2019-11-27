4e29483a12694fff6dc7211c71be85e1eedb2b8e
ivandi1980 <ivandi.djoh@gmail.com>
2019-11-27 08:17:24 +0700

    modified header and replace captured

--- src/views/Index/Index.php
+++ src/views/Index/Index.php
@@ -2,7 +2,8 @@
 <div class="container">
     <div class="jumbotron">
         <h3 class="display-5">DISBURSEMENT</h3>
-        <p class="lead">This is a Small Services that Connected to 3rd party is called slightly-big Flip. The purpose of this Services is to send the disbursement data to the 3rd party API (slightly-big Flip).</p>
+        <p class="lead">This is a Small Services that Connected to 3rd party API called slightly-big Flip. 
+        <br />The purpose of this Services is to send the disbursement data to the 3rd party API (slightly-big Flip) & save it to the DB</p>
         <hr class="my-4">
         <p class="lead">
             <a class="btn btn-primary btn-sm" href="<?= URLROOT; ?>/public/list" role="button">Preview Disbursement</a>
--- src/views/include/header.php
+++ src/views/include/header.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         <a class="navbar-brand" href="<?= URLROOT; ?>/public">DISBURSEMENT</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
